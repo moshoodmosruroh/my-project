7f8d354df5e0fa766ee847d137edd4008c075191
moshoodmosruroh <moshoodmosruroh@gmail.com>
2024-11-26 15:00:33 +0100

    my second git

---
+++ firstclass/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Hello World");
+int myNum;
+myNum = 50;
+Console.WriteLine(myNum); 
+int myInt;
+myInt = 10;
+Console.WriteLine(myInt);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
